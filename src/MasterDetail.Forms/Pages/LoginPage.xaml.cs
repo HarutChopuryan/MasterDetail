297121fb1b80ef8b02dda519dcd90efa3b3fcdff
Harut Chopuryan <h_chopuryan@bk.ru>
2018-09-04 17:21:57 +0400

    Added Dropbox log in(...)

--- src/MasterDetail.Forms/Pages/LoginPage.xaml.cs
+++ src/MasterDetail.Forms/Pages/LoginPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dropbox.Api;
 using MasterDetail.Core.DI;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,8 +22,23 @@ namespace MasterDetail.Forms.Pages
 
 	    private async void OnSigninClicked(object sender, EventArgs e)
 	    {
+	        //string _accessKey = "Qg1P2iJ2DrAAAAAAAAAADLjGU5TlXqZgqTbejadackNzMBEkVrWO86BPK5qLNrX9";
+         //   var task = Task.Run(async () =>
+	        //{
+	        //    using (var dbx = new DropboxClient(_accessKey))
+	        //    {
+	        //        var full = await dbx.Users.GetCurrentAccountAsync();
+	        //        if (full != null)
+	        //        {
+	        //            Debug.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+	        //            var userPage = ServiceLocator.Instance.Locate<MasterDetailMainPage>();
+	        //            await Navigation.PushAsync(userPage);
+	        //        }
+	        //    }
+         //   });
+
 	        var userPage = ServiceLocator.Instance.Locate<MasterDetailMainPage>();
 	        await Navigation.PushAsync(userPage);
-	    }
+        }
 	}
 }
