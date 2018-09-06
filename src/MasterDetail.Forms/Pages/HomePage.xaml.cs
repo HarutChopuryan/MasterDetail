5b28144594d07fbb8498236e0e4795bbf4b52f2d
Harut Chopuryan <h_chopuryan@bk.ru>
2018-09-06 19:31:03 +0400

    Showing added images in ListView mod

--- src/MasterDetail.Forms/Pages/HomePage.xaml.cs
+++ src/MasterDetail.Forms/Pages/HomePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MasterDetail.UI.Main;
+﻿using System.IO;
+using Dropbox.Api;
+using Dropbox.Api.Files;
+using MasterDetail.UI.Main;
 using MasterDetail.UI.Main.Implementation;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,5 +19,34 @@ namespace MasterDetail.Forms.Pages
             InitializeComponent();
             BindingContext = _viewModel;
         }
+
+        //protected override async void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    var _accessKey = "Qg1P2iJ2DrAAAAAAAAAADLjGU5TlXqZgqTbejadackNzMBEkVrWO86BPK5qLNrX9";
+
+        //    using (var client = new DropboxClient(_accessKey))
+        //    {
+        //        var list = await client.Files.ListFolderAsync(string.Empty);
+
+        //        foreach (var item in list.Entries)
+        //        {
+        //            if (!item.IsFile || !item.Name.EndsWith(".jpg") && !item.Name.EndsWith(".png")) continue;
+
+        //            Stream s = new MemoryStream();
+        //            var resp = await client.Files.DownloadAsync("/nature.jpg");
+        //            resp.GetContentAsStreamAsync().Result.CopyTo(s);
+        //            Stream ds = await resp.GetContentAsStreamAsync();
+        //            await ds.CopyToAsync(s);
+        //            ds.Dispose();
+
+        //            _viewModel.ImgItems.Add(new UserImagesViewModel
+        //            {
+        //                ImageSource = ImageSource.FromStream(()=>s),
+        //                ImageName = item.Name
+        //            });
+        //        }
+        //    }
+        //}
     }
 }
